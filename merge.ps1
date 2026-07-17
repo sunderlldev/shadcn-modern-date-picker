a24bcd76557e9626fb3fe7a92172036d4f8a953b
Juan Blas <juanjoseblas344@gmail.com>
2026-07-17 02:12:02 -0500

    chore: cleanup

--- merge.ps1
+++
@@ -1,5 +0,0 @@
-git add .
-git commit -m "docs: update repository name and links to shadcn-modern-date-picker"
-git remote add origin https://github.com/sunderlldev/shadcn-modern-date-picker.git
-git fetch
-git merge origin/main --allow-unrelated-histories -m "chore: merge LICENSE from remote repository"
